af62aceef433d66041b77940fafb4b74b8a9cf1b
guoyydev <gyy_Android@163.com>
2017-05-17 18:11:37 +0800

    增加后台自动更新天气功能

--- app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -1,9 +1,12 @@
 package com.coolweather.android.service;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 
 import com.coolweather.android.gson.Weather;
@@ -30,6 +33,13 @@ public class AutoUpdateService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         updateWeather();
         updateBingPic();
+        AlarmManager manager= (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour=8*60*60*1000;//这是8小时的毫秒数
+        long triggerAtTime= SystemClock.elapsedRealtime()+anHour;
+        Intent i=new Intent(this,AutoUpdateService.class);
+        PendingIntent pi=PendingIntent.getService(this,0,i,0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
         return super.onStartCommand(intent, flags, startId);
     }
 
